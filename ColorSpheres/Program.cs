b61c894658a15005dd8833746db886cfdce6a68f
Leonardo Henrique <leonardojose@netcabo.pt>
2024-04-12 14:14:42 +0100

    Estado representado pelos valores pedidos

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Color
+{
+
+    int red;
+    int green;
+    int blue;
+    int alpha;
+
+    Color(int red, int green, int blue, int alpha)
+    {
+        // Verifica se os valores estão dentro dos parâmetros
+        this.red = Clamp(red, 0, 255);
+        this.green = Clamp(green, 0, 255);
+        this.blue = Clamp(blue, 0, 255);
+        this.alpha = Clamp(alpha, 0, 255);
+    }
+
+    // Método para a cor Vermelha
+    int GetRed()
+    {
+        return red;
+    }
+
+    // Método para a cor Verde
+    int GetGreen()
+    {
+        return green;
+    }
+
+    // Método para a cor Azul
+    int GetBlue()
+    {
+        return blue;
+    }
+
+    // Método para o Alpha
+    int GetAlpha()
+    {
+        return alpha;
+    }
+
+
+
+
+}
