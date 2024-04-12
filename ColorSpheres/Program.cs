c317ffb773bff3ea45eb7da51f15a6aec2f4818b
Leonardo Henrique <leonardojose@netcabo.pt>
2024-04-12 14:36:24 +0100

    Construtor

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -2,13 +2,13 @@
 
 class Color
 {
-
     int red;
     int green;
     int blue;
     int alpha;
 
-    Color(int red, int green, int blue, int alpha)
+    // Construtor que aceita todos os parâmetros necessários para inicializar o estado de uma cor
+    public Color(int red, int green, int blue, int alpha)
     {
         // Verifica se os valores estão dentro dos parâmetros
         this.red = Clamp(red, 0, 255);
@@ -41,7 +41,14 @@ class Color
         return alpha;
     }
 
-
-
-
+    // Método para garantir que os valores estejam dentro dos limites permitidos
+    int Clamp(int value, int min, int max)
+    {
+        if (value < min)
+            return min;
+        else if (value > max)
+            return max;
+        else
+            return value;
+    }
 }
